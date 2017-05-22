1be34fd4c4c2c8798dd2e4a609f5dde3ce824d01
leontaolong <longt8@uw.edu>
2017-05-21 17:44:00 -0700

    Add a custom TaskAdapter for list_item_task layout

---
+++ CheckMate/app/src/main/java/edu/uw/dengz6/checkmate/TaskAdapter.java
@@ -0,0 +1,72 @@
+package edu.uw.dengz6.checkmate;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * An custom array adapter for Task Object
+ */
+
+public class TaskAdapter extends ArrayAdapter<TaskAdapter.TaskData> {
+
+    public static class TaskData {
+        public String title;
+        public String details;
+        public Date dueOn;
+        public Date createdOn;
+        public String assigner;
+        public String assignee;
+
+        public TaskData(String title, String details, Date dueOn, Date createdOn, String assigner, String assignee) {
+            this.title = title;
+            this.details = details;
+            this.dueOn = dueOn;
+            this.createdOn = createdOn;
+            this.assigner = assigner;
+            this.assignee = assignee;
+        }
+    }
+
+    public static class ViewHolder {
+        TextView title;
+        TextView dueOn;
+        TextView details;
+        TextView assignee;
+        int position;
+    }
+
+    public TaskAdapter(Context context, ArrayList<TaskData> data) {
+        super(context, 0, data);
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the data item for this position
+        TaskData data = getItem(position);
+        ViewHolder holder;
+
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item_task, parent, false);
+            holder = new ViewHolder();
+            holder.title = (TextView) convertView.findViewById(R.id.task_item_title);
+            // TODO: complete the rest view binding
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        holder.title.setText(data.title);
+        // TODO: complete the rest view binding
+        // Return the completed view to render on screen
+        return convertView;
+    }
+}
