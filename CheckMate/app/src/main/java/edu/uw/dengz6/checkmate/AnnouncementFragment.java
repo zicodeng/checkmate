65114071c662edfa2c7b31b010c5c9075c4e33e9
Allen Cho <allencho@uw.edu>
2017-05-23 21:39:57 -0700

    still on process

--- CheckMate/app/src/main/java/edu/uw/dengz6/checkmate/AnnouncementFragment.java
+++ CheckMate/app/src/main/java/edu/uw/dengz6/checkmate/AnnouncementFragment.java
@@ -1,13 +1,18 @@
 package edu.uw.dengz6.checkmate;
 
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.Toast;
 
 
@@ -42,9 +47,9 @@ public class AnnouncementFragment extends Fragment {
         fabAllTasks.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                AlertDialog.Builder mBuilder = new AlertDialog.Builder(getContext());
-                EditText mTitle = (EditText) m
                 Toast.makeText(getActivity(), "Add a New Announcement", Toast.LENGTH_SHORT).show();
+                DialogFragment addNewAnnouncementFragment = AddNewAnnouncementFragment.newInstance();
+                addNewAnnouncementFragment.show(getActivity().getSupportFragmentManager(), "Add_New_Shopping_List_Fragment");
             }
         });
 
@@ -52,5 +57,52 @@ public class AnnouncementFragment extends Fragment {
         return rootView;
     }
 
+    public static class AddNewAnnouncementFragment extends DialogFragment {
+
+        public static AddNewAnnouncementFragment newInstance() {
+
+            Bundle args = new Bundle();
+
+            AddNewAnnouncementFragment fragment = new AddNewAnnouncementFragment();
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+            builder.setTitle("Add a New Announcement");
+
+            View viewInflated = LayoutInflater.from(getContext()).inflate(R.layout.fragment_add_new_announcement, (ViewGroup) getView(), false);
+
+            // Set up the input
+            final EditText title = (EditText) viewInflated.findViewById(R.id.textTitle);
+            final EditText description = (EditText) viewInflated.findViewById(R.id.textDescription);
+
+            // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+            builder.setView(viewInflated);
+
+            // Set up the buttons
+            builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                    String getTitle = title.getText().toString();
+                    String getDescription = description.getText().toString();
+                }
+            });
+
+            builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+
+            return builder.create();
+        }
+    }
 }
 
