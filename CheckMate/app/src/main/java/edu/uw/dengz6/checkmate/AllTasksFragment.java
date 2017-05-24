36adb2eef86cf19cb34c940b686108c1907fcf1e
leontaolong <longt8@uw.edu>
2017-05-23 23:42:23 -0700

    Implement data binding to firebase

--- CheckMate/app/src/main/java/edu/uw/dengz6/checkmate/AllTasksFragment.java
+++ CheckMate/app/src/main/java/edu/uw/dengz6/checkmate/AllTasksFragment.java
@@ -1,12 +1,18 @@
 package edu.uw.dengz6.checkmate;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.firebase.client.DataSnapshot;
@@ -14,6 +20,8 @@ import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 
 
@@ -25,6 +33,10 @@ public class AllTasksFragment extends Fragment {
     public static final String FIREBASE_URL = "https://checkmate-d2c41.firebaseio.com/groups/";
 
     public static final String TAG = "All_Tasks_Fragment";
+    protected static SessionManager manager;
+    protected static HashMap<String, String> userInfo;
+    protected static Firebase taskRef;
+
     public AllTasksFragment() {
         // Required empty public constructor
     }
@@ -53,16 +65,28 @@ public class AllTasksFragment extends Fragment {
             }
         });
 
-        SessionManager manager = new SessionManager(getContext());
-        HashMap<String, String> userInfo = manager.getUserDetails();
+        fabAllTasks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Add a New Task", Toast.LENGTH_SHORT).show();
+
+                // Create a dialog and ask the user for input
+                DialogFragment AddNewTaskFragment = AllTasksFragment.AddNewTaskFragment.newInstance();
+                AddNewTaskFragment.show(getActivity().getSupportFragmentManager(), "Add_New_Task");
+            }
+        });
+        manager = new SessionManager(getContext());
+        userInfo = manager.getUserDetails();
 
         Firebase.setAndroidContext(getActivity());
-        Firebase taskRef = new Firebase(FIREBASE_URL + userInfo.get(SessionManager.KEY_GROUP_NAME) + "/users");
+        taskRef = new Firebase(FIREBASE_URL + userInfo.get(SessionManager.KEY_GROUP_NAME) + "/tasks");
+
         taskRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                 Log.v(TAG, dataSnapshot.getValue().toString());
-
+                if (dataSnapshot.getValue() != null) {
+                    Log.v(TAG, dataSnapshot.getValue().toString());
+                }
             }
             @Override
             public void onCancelled(FirebaseError firebaseError) {
@@ -73,5 +97,62 @@ public class AllTasksFragment extends Fragment {
         // Inflate the layout for this fragment
         return rootView;
     };
+
+
+    public static class AddNewTaskFragment extends DialogFragment {
+
+        public static AddNewTaskFragment newInstance() {
+
+            Bundle args = new Bundle();
+
+            AddNewTaskFragment fragment = new AddNewTaskFragment();
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+            builder.setTitle("Add a New Task");
+
+            View viewInflated = LayoutInflater.from(getContext()).inflate(R.layout.add_new_task, (ViewGroup) getView(), false);
+
+            // Set up the input
+            final EditText taskTitle = (EditText) viewInflated.findViewById(R.id.task_title);
+            final EditText taskDetail = (EditText) viewInflated.findViewById(R.id.task_detail);
+
+            // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+            builder.setView(viewInflated);
+
+            // Set up the buttons
+            builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Firebase taskRef = AllTasksFragment.taskRef;
+
+                    Firebase mTask = taskRef.push();
+                    // TODO: roommate dropdown and date picker
+                    dialog.dismiss();
+                    String title = taskTitle.getText().toString();
+                    String detail = taskDetail.getText().toString();
+                    SimpleDateFormat dt = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+                    String createdOn = dt.format(new Date());
+                    String assigner = AllTasksFragment.userInfo.get(SessionManager.KEY_USER_ID);
+                    mTask.setValue(new TaskData(title, detail, createdOn, createdOn, assigner, assigner));
+                }
+            });
+
+            builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+
+            return builder.create();
+        }
+    }
 
 }
--- CheckMate/app/src/main/java/edu/uw/dengz6/checkmate/TaskData.java
+++ CheckMate/app/src/main/java/edu/uw/dengz6/checkmate/TaskData.java
@@ -2,15 +2,15 @@ package edu.uw.dengz6.checkmate;
 
 public class TaskData {
     public String title;
-    public String details;
+    public String detail;
     public String dueOn;
     public String createdOn;
     public String assigner;
     public String assignee;
 
-    public TaskData(String title, String details, String dueOn, String createdOn, String assigner, String assignee) {
+    public TaskData(String title, String detail, String dueOn, String createdOn, String assigner, String assignee) {
         this.title = title;
-        this.details = details;
+        this.detail = detail;
         this.dueOn = dueOn;
         this.createdOn = createdOn;
         this.assigner = assigner;
