b06a9cb240fab58c91d58346a20e0b3f2d493bd8
Quan <minhquan611996@gmail.com>
2017-05-31 01:04:59 -0700

    working

--- CheckMate/app/src/main/java/edu/uw/dengz6/checkmate/ReminderBroadcastReceiver.java
+++ CheckMate/app/src/main/java/edu/uw/dengz6/checkmate/ReminderBroadcastReceiver.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.app.TaskStackBuilder;
 
 public class ReminderBroadcastReceiver extends BroadcastReceiver {
     public ReminderBroadcastReceiver() {
@@ -13,13 +14,23 @@ public class ReminderBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        if(intent.getExtras() != null){
+
+        }
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
                 .setSmallIcon(R.drawable.ic_checkmate_logo)
                 .setContentTitle("Task reminder")
-                .setContentText("task reminder");
+                .setContentText("task reminder")
+                .setAutoCancel(true);
         Intent intentToFire = new Intent(context, MainActivity.class);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        stackBuilder.addParentStack(MainActivity.class);
+        stackBuilder.addNextIntent(intentToFire);
+
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intentToFire, PendingIntent.FLAG_UPDATE_CURRENT);
         builder.setContentIntent(pendingIntent);
+
         NotificationManagerCompat.from(context).notify((int) System.currentTimeMillis(), builder.build());
     }
 }
